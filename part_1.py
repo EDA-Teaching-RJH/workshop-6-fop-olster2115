c5a13a54d6fdb53bf048b563c801ba6c97bcdb06
olster2115 <ot210@kent.ac.uk>
2026-02-10 13:49:42 +0000

    Part 1-1.21

--- part_1.py
+++ part_1.py
@@ -1,1 +1,14 @@
+# 1.1
+sample_bay_list = ["Basalt", "Silica", "Iron", "Dust"]   
+# 1.12
+print(sample_bay_list[0])
+# 1.13
+last_item = sample_bay_list[-1]
 
+print(last_item)
+# 1.14
+all_sample_bay = len(sample_bay_list)
+
+print(all_sample_bay)
+# 1.21
+for sample in sample_bay_list :
